c620ac64c5e44ac78169d738f4e72c413cb33852
Danchek <denisdotsenko7@gmail.com>
2016-11-09 17:52:30 +0200

    first init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NestedForms::Application.config.secret_token = '22a6fc1b2fd7aa87e45e44133992abb2170659fa17aff9cfd7ab3a29e8713e22631ec11c48fea2087a454ab294f49fae5e022eeb78af637c18e8f5d1912de0e8'
